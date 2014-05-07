b406d51ba507621e9e20f33dd765ae3e7d54a82d
Dan Schwarz <DSchwarz@YVR-WS355.visier.corp>
2014-05-07 11:52:15 -0700

    Successfully scrapes

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,21 +2,29 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.libs.ws._
 
-import play.
+import scala.concurrent.Future
+import scala.util.{Success, Failure}
+
+import scala.concurrent.ExecutionContext.Implicits.global
 
 object Application extends Controller {
 
 	def index = Action {
+		scrape("http://docs.scala-lang.org/overviews/core/futures.html")
 		Ok(views.html.index("Your new application is ready."))
 	}
 
 	def scrape(url: String) = {
-		val webPage: Future[libs.ws.Response] = WS.url(url).get().getBody()
-		Async {
-
-			val linkPattern = """<a\s+(?:[^>]*\s+)?href="([^"]*)"[^<]*</a>""".r
-			val links = linkPattern.findAllMatchIn(webpage).toArray.map({ m => m.group(1)})
+		val webPage = WS.url(url).get
+		webPage onComplete {
+			case Success(page) => {
+				val linkPattern = """<a\s+(?:[^>]*\s+)?href="(http[^:]*:[^"]*)"[^<]*</a>""".r
+				val links = linkPattern.findAllMatchIn( page.body ).toArray.map({ m => m.group(1)})
+				for (link <- links) { println(link) }
+			}
+			case Failure(t) => println("An error has occurred: " + t.getMessage)
 		}
 
 	}
